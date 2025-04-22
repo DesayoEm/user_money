6e475cca7ac14f5059456508f3a2ea708d1468bf
Adesayo M Labaeka <labaekaadesayo@gmail.com>
2025-04-22 02:02:15 +0100

    switched to MongoDB Atlas and updated server config

--- app/database.py
+++ app/database.py
@@ -7,6 +7,6 @@ load_dotenv()
 
 client = mongo_client.MongoClient(os.environ.get("DB_URL"))
 
-accounts_collection = client["user_money"]["accounts"]
-users_collection = client["user_money"]["users"]
-transactions_collection = client["user_money"]["transactions"]
+accounts_collection = client["user-money"]["accounts"]
+users_collection = client["user-money"]["users"]
+transactions_collection = client["user-money"]["transactions"]
